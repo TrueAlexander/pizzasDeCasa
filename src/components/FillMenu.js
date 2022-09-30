7ef7f238e050e23cdf50e6e2ff6284c6d33e2907
Alexander <caminante.msk@gmail.com>
2022-09-30 16:17:25 -0300

    added loading from Firebase

--- src/components/FillMenu.js
+++ src/components/FillMenu.js
@@ -1,9 +1,28 @@
-import dataProducts from "../dataProducts.json"
+// import dataProducts from "./../dataProducts.json"
 import Product from "./Product"
 import 'animate.css'
+import { useEffect, useState } from "react"
 
 const FillMenu = () => {
-  return dataProducts.map((el) => {
+
+  const [dataProducts, setDataProducts] = useState([])
+  //get data from Firebase DB
+  const getDataProducts = async () => {
+    await fetch('https://pizzas-de-casa-default-rtdb.firebaseio.com/dataProducts.json')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      return setDataProducts(data)
+    })
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    return setDataProducts(getDataProducts())
+  }, [])
+
+  return dataProducts.length > 0 ? dataProducts.map((el) => {
       return (<div key={el.id} className="home__card">
                 <Product 
                   title={el.title} 
@@ -14,8 +33,8 @@ const FillMenu = () => {
                   img={el.img} 
                   id={el.id}
                 />
-              </div>)
-  })
+              </div>) 
+  }) : <h3 className="home__loading">carregando...</h3>
 }
 
 export default FillMenu
